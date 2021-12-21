0bc7504cc48a736de939ba4820c9d47e33c0470d
Jacob Shandling <jacobshandling@icloud.com>
2021-12-20 20:52:47 -0800

    Scaffold user reg/auth views

--- backend/seeds/views.py
+++ backend/seeds/views.py
@@ -1,5 +1,70 @@
+from typing import *
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import User
 
 def index(request):
     return render(request, 'seeds/index.html')
 
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            # TODO: modify below line to work with REST / frontend setup
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            # TODO: modify below to work with REST / frontend setup
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else: 
+        # TODO: modify below to work with REST / frontend setup
+        return render(request, "auctions/login.html")
+
+def logout_view(request):
+    logout(request)
+    # TODO: modify below to work with REST / frontend setup
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            # TODO: modify below to work with REST / frontend setup
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            # TODO: modify below to work with REST / frontend setup
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        # TODO: modify below to work with REST / frontend setup
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        # TODO: modify below to work with REST / frontend setup
+        return render(request, "auctions/register.html"
