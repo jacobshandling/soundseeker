8907731acde2d043a28820a1be28006db27c9144
Jacob Shandling <jacobshandling@icloud.com>
2021-12-20 16:30:55 -0800

    set up webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+// frontend/webpack.config.js
+
+const path = require('path')
+const webpack = require('webpack')
+module.exports = {
+  // Where Webpack looks to load your JavaScript
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
+  mode: 'development',
+  // Where Webpack spits out the results
+  output: {
+    path: path.resolve(__dirname, '../backend/seeds/static/seeds/build'),
+    filename: '[name].js',
+  },
+  plugins: [
+    // Don't output new files if there is an error
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  // Where find modules that can be imported (eg. React) 
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    modules: [
+        path.resolve(__dirname, 'src'),
+        path.resolve(__dirname, 'node_modules'),
+    ],
+  },
+}
