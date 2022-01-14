66e64a383603ae2240b5319b97ddfdc8e8709aa5
Jacob Shandling <jacobshandling@pm.me>
2022-01-13 16:08:07 -0800

    Update serializers to include all info nested in root API route

--- backend/seeds/serializers.py
+++ backend/seeds/serializers.py
@@ -1,16 +1,19 @@
 from rest_framework import serializers
 from seeds.models import *
 
+# https://www.django-rest-framework.org/api-guide/serializers/#dealing-with-nested-objects
+
 class AudioClipSerializer(serializers.HyperlinkedModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    # user = serializers.ReadOnlyField(source='user.username')
     file = serializers.HyperlinkedIdentityField(view_name='audio-file', format='mp3')  # TODO: How to server audio ?!
     
     class Meta:
         model = AudioClip
-        fields = ['url', 'id', 'user', 'name', 'file']
+        fields = ['url', 'id', 'user', 'name']
 
 class BlobSerializer(serializers.HyperlinkedModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    # user = serializers.ReadOnlyField(source='user.username')
+    clips = AudioClipSerializer(many=True, required=False)
     
     class Meta:
         model = Blob
@@ -18,7 +21,8 @@ class BlobSerializer(serializers.HyperlinkedModelSerializer):
 
 class SuiteSerializer(serializers.HyperlinkedModelSerializer):
     # user = serializers.ReadOnlyField(source='user.username')
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), source='user.id')
+    # user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), source='user.id')
+    blobs = BlobSerializer(many=True)
 
     class Meta:
         model = Suite
@@ -26,7 +30,7 @@ class SuiteSerializer(serializers.HyperlinkedModelSerializer):
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     # user_suites = serializers.HyperlinkedRelatedField(many=True, view_name='suite-detail', read_only=True)
-    user_suites = SuiteSerializer(many=True, read_only=True)
+    user_suites = SuiteSerializer(many=True, read_only=True, required=False)
 
     class Meta:
         model = User
