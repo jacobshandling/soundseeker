e98286dfd16c8e265d6e9d651cf7eca3e61a26b4
Jacob Shandling <jacobshandling@pm.me>
2022-03-19 15:16:05 -0700

    Style upload view

--- frontend/src/FileUploadView.js
+++ frontend/src/FileUploadView.js
@@ -7,7 +7,7 @@ class FileUploadView extends React.Component {
             blobOptions.push(
                     <label htmlFor={userBlob.name} key={userBlob.id}>
                         <input id={userBlob.name} className="blob-select-checkbox" key={userBlob.id} type="checkbox" name="blob-options" value={userBlob.id} />
-                    {userBlob.name}</label>
+                    <p>{userBlob.name}</p></label>
                     
             )
         });
@@ -21,7 +21,7 @@ class FileUploadView extends React.Component {
                 <input id="selected-file" className="upload-field" type="file" onChange={this.props.onFileSelect} required/>
                 <input id="clip-name" className="upload-field" type="text" placeholder='clip name' />
                 <div className="upload-field" id="blob-options">
-                    <p>Blobs to associate this clip with:</p> 
+                    <h3>Blobs to associate this clip with:</h3> 
                     {blobOptions}
                 </div>
                 <input type="submit" className="upload-field" onClick={this.props.onFileUpload} value="Upload" />
