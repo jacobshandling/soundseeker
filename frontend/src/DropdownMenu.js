f7e19c7b3c04e52d39347506ea0b847fdae7efcf
Jacob Shandling <jacobshandling@pm.me>
2022-03-19 15:17:34 -0700

    Make clicking on upload a clip more logical

--- frontend/src/DropdownMenu.js
+++ frontend/src/DropdownMenu.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function DropdownItem(props) {
     return (
-        <a href="#" className="menu-item" onClick={props.toggleClipUpload}>
+        <a href="#" className="menu-item" onClick={props.onClick}>
             <h4>{props.children}</h4>
         </a>
     )
@@ -14,7 +14,7 @@ class DropdownMenu extends React.Component {
     render() {
         return(
             <div className="dropdown">
-                <DropdownItem toggleClipUpload={this.props.toggleClipUpload}>
+                <DropdownItem onClick={this.props.toggleClipUpload}>
                     Upload a Clip
                 </DropdownItem>
                 <DropdownItem>Create a Blob</DropdownItem>
