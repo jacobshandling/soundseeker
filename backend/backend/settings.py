cf3cd898c14ddf128db6c2817052857db94aa177
Jacob Shandling <jacobshandling@pm.me>
2022-04-01 19:12:32 -0700

    Remove redundant definition

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
 
 
 ROOT_URLCONF = 'backend.urls'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
@@ -126,10 +129,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
