277bbe12a859a0d418076b166c1ab0fd2338eadc
Jacob Shandling <jacobshandling@pm.me>
2022-04-19 22:30:43 -0700

    Create EditSuiteView

---
+++ frontend/src/EditSuiteView.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class EditSuiteView extends React.Component {
+
+    renderBlobOptions(curSuite) {
+        const blobOptions = [];
+        curSuite.blobs.forEach(blob => {
+            blobOptions.push(
+                    <label htmlFor={blob.name} key={blob.id}>
+                        <input id={blob.id} className="select-checkbox" key={blob.id} type="checkbox" name="blob-options" value={blob.url} checked />
+                    <p>{blob.name}</p></label>
+                    
+            )
+        });
+        return blobOptions;
+    }
+
+    render() {
+        const curSuite = this.props.suite;
+        const blobOptions = renderBlobOptions(curSuite);
+        return(
+            <section id="edit-suite">
+                <form id="edit-suite-form">
+                    <input id="new-suite-name" className="form-field" type="text" placeholder={curSuite.name} />
+                    <fieldset className="form-field" id="new-blob-associations">
+                        <legend>Blobs of this Suite (at least 1):</legend> 
+                        {blobOptions}
+                    </fieldset>
+                    <input type="submit" className="form-field" onClick={this.props.onEditSuite} value="Submit" />
+                </form>
+                <button onClick={this.props.handleDeleteSuite(curSuite)}>Delete this Suite</button>
+            </section>
+        )
+    }
+
+}
+
+export default CreateSuiteView;
