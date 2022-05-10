1fd702720d8aa084061c5716a8d06e1c7ea49d49
Jacob Shandling <jacobshandling@pm.me>
2022-05-10 15:17:46 -0700

    set default permissions on dev settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,6 +53,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
+}
+
 ROOT_URLCONF = 'backend.urls'
 
 # Static files (CSS, JavaScript, Images)
