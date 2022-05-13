d9fd38e550ce023a0443d1ff8d3cd6525c84aa3b
Jacob Shandling <jacobshandling@pm.me>
2022-05-12 22:25:36 -0700

    turn on debug

--- backend/backend/heroku_settings.py
+++ backend/backend/heroku_settings.py
@@ -36,7 +36,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SOUNDSEEKER_SERVER_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sound-seeker.herokuapp.com']
 
