d26fde918027743cbcf51b4e2497bc9f21b4c185
Jacob Shandling <jacobshandling@pm.me>
2022-05-18 18:32:36 -0700

    Remove edit form from EditClipView, to include again once onEditClip fixed

--- frontend/src/EditClipView.js
+++ frontend/src/EditClipView.js
@@ -7,11 +7,11 @@ class EditClipView extends React.Component {
         return(
             <section id="edit-clip" className="action-view">
                 <h3>Edit clip "{clip.name}"</h3>
-                <div id="edit-clip-form" className="form">
+                {/* <div id="edit-clip-form" className="form">
                     <legend>New name:</legend>
                     <input id="new-name" className="form-field" type="text" defaultValue={clip.name} />
                     <input type="button" className="form-field" onClick={this.props.onEditClip} value="Submit" />
-                </div>
+                </div> */}
                 <button onClick={this.props.onDeleteClip}>Delete Clip"{clip.name}"</button>
             </section>
         )
