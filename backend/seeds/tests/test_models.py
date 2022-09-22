df7642989f2f411b3189a6b3c76e2ac3de8a6bea
Jacob Shandling <jacobshandling@icloud.com>
2022-09-22 12:25:56 -0700

    try with SuiteTests - better approach?

--- backend/seeds/tests/test_models.py
+++ backend/seeds/tests/test_models.py
@@ -4,12 +4,15 @@ from seeds.models import Blob, Suite, User
 
 class UserTests(TestCase):
     def setUp(self):
-        User.objects.create(username="test_user")
+        self.test_user = User.objects.create(username="test_user")
+
+    def tearDown(self):
+        self.test_user.delete()
 
     def test_user_exists_with_name(self):
-        test_user = User.objects.get(username="test_user")
-        self.assertIsNotNone(test_user)
-        self.assertEqual(test_user.get_username(), "test_user")
+        # test_user = User.objects.get(username="test_user")
+        self.assertIsNotNone(self.test_user)
+        self.assertEqual(self.test_user.get_username(), "test_user")
 
 
 class SuiteTests(TestCase):
@@ -17,27 +20,27 @@ class SuiteTests(TestCase):
         """
         Initialize all Suite tests with new Suite and User records
         """
-        test_user = User.objects.create(username="test_user")
-        Suite.objects.create(name="test_suite", owner=test_user)
+        self.test_user = User.objects.create(username="test_user")
+        self.test_suite = Suite.objects.create(name="test_suite", owner=self.test_user)
+
+    def tearDown(self):
+        self.test_user.delete()
+        self.test_suite.delete()
 
     def test_suite_exists_with_name_and_owner(self):
-        test_user = User.objects.get(username="test_user")
-        test_suite = Suite.objects.get(name="test_suite")
-        self.assertIsNotNone(test_suite)
-        self.assertEqual(test_suite.name, "test_suite")
-        self.assertEqual(test_suite.owner, test_user)
+        self.assertIsNotNone(self.test_suite)
+        self.assertEqual(self.test_suite.name, "test_suite")
+        self.assertEqual(self.test_suite.owner, self.test_user)
 
     def test_suite_has_no_blobs(self):
-        test_suite = Suite.objects.get(name="test_suite")
-        self.assertEqual(test_suite.blobs.count(), 0)
+        self.assertEqual(self.test_suite.blobs.count(), 0)
 
     def test_add_blob_to_suite(self):
-        test_suite = Suite.objects.get(name="test_suite")
         # create a new blob and add it to suite's blobs
-        test_blob = test_suite.blobs.create(name="test_blob", owner=test_suite.owner)
+        test_blob = self.test_suite.blobs.create(name="test_blob", owner=self.test_user)
         # test that this relationship exists from both directions
-        self.assertEqual(test_suite.blobs.get(name="test_blob"), test_blob)
-        self.assertEqual(test_blob.suites.get(name="test_suite"), test_suite)
+        self.assertEqual(self.test_suite.blobs.get(name="test_blob"), test_blob)
+        self.assertEqual(test_blob.suites.get(name="test_suite"), self.test_suite)
 
 
 class BlobTests(TestCase):
