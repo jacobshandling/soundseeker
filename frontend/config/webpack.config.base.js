30862f33eb97ee357129bcd27fa253f09a69ea4a
Jacob Shandling <jacobshandling@icloud.com>
2022-09-28 13:16:07 -0700

    move webpack config into single dir; remove unused Dotenv from webpack config

--- frontend/config/webpack.config.base.js
+++ frontend/config/webpack.config.base.js
@@ -1,27 +1,24 @@
 const path = require('path');
 const webpack = require('webpack');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, 'src', 'index.js'),
+        main: path.resolve(__dirname, '..', 'src', 'index.js'),
     },
     plugins: [
         // Don't output new files if there is an error
         new webpack.NoEmitOnErrorsPlugin(),
-        // use Dotenv to access environment vars with process.env.ENV_VAR
-        new Dotenv()
     ],
     output: {
-        path: path.resolve(__dirname, '..', 'backend', 'seeds', 'static','seeds', 'build'),
+        path: path.resolve(__dirname, '..', '..', 'backend', 'seeds', 'static','seeds', 'build'),
     },
 
     // Where to find modules that can be imported (eg. React)
     resolve: {
         extensions: ['*', '.js', '.jsx'],
         modules: [
-            path.resolve(__dirname, 'src'),
-            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '..', 'src'),
+            path.resolve(__dirname, '..', 'node_modules'),
         ],
     },
 
